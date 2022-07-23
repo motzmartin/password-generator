b0d80f2a70646114e9430f2585ef851dd0d76319
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-07-23 12:44:47 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,23 @@
+#include <sodium.h>
 #include <iostream>
 #include <string>
-#include <sodium.h>
 
-#pragma comment(lib, "libsodium.lib")
-
-int main()
+int main(void)
 {
 	if (sodium_init() == -1)
 	{
-		std::cout << "sodium_init() failed" << std::endl;
 		return 1;
 	}
 
 	std::string base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-	std::string password = "";
+	std::string password;
 
 	for (int i = 0; i < 20; i++)
 	{
 		if (i == 6 || i == 13)
 		{
-			password += "-";
+			password += '-';
 		}
 		else
 		{
@@ -28,20 +25,7 @@ int main()
 		}
 	}
 
-	char hash[crypto_pwhash_STRBYTES];
-
-	if (crypto_pwhash_str(hash,
-		password.c_str(),
-		password.size(),
-		crypto_pwhash_OPSLIMIT_SENSITIVE,
-		crypto_pwhash_MEMLIMIT_SENSITIVE) == -1)
-	{
-		std::cout << "crypto_pwhash_str() failed" << std::endl;
-		return 1;
-	}
-
-	std::cout << "password: " << password << std::endl;
-	std::cout << "hash: " << hash << std::endl;
+	std::cout << password << std::endl;
 
 	return 0;
 }
